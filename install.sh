b83fc4ee4ae1b1fc8bd9aedc8fc35fbf7fac0813
bob16795 <prestosilver.ptp@gmail.com>
2022-11-18 10:25:44 -0500

    change to asm file

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 pushd /tmp
 curl https://raw.githubusercontent.com/slam-lang/spakig/master/spakig.fasm -o spakig.fasm
-fasm -m 524288 spakig.fasm temp.o
+fasm -m 524288 spakig.asm temp.o
 ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 -o ./spakig -lc -melf_x86_64 temp.o
 ./spakig init

7bc3a0a530e1cf0bd0f541db93ac1b1c3bf973f8
bob16795 <prestosilver.ptp@gmail.com>
2022-11-18 10:26:57 -0500

    fix install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd /tmp
-curl https://raw.githubusercontent.com/slam-lang/spakig/master/spakig.fasm -o spakig.fasm
+curl https://raw.githubusercontent.com/slam-lang/spakig/master/spakig.asm -o spakig.asm
 fasm -m 524288 spakig.asm temp.o
 ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 -o ./spakig -lc -melf_x86_64 temp.o
 ./spakig init

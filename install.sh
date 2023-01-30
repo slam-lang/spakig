f931eb261bf18794fd81f2e2ce2a10dba99631e6
bob16795 <prestosilver.ptp@gmail.com>
2023-01-29 21:25:37 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+[[ "$(uname -m)" != "x86_64" ]] && {
+    echo "Slam can only run on 64 bit linux at the moment"
+    exit
+}
+
+which fasm || {
+    echo "Slam requires fasm to install"
+    exit
+}
+
 pushd /tmp
 curl https://raw.githubusercontent.com/slam-lang/spakig/master/spakig.asm -o spakig.asm
 fasm -m 524288 spakig.asm temp.o
 ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 -o ./spakig -lc -melf_x86_64 temp.o
 ./spakig init
+popd
